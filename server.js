896f107cb96be51d309d934c18066356a2d46085
akashg7 <akashg250804@gmail.com>
2024-11-25 16:06:15 +0530

    made some changes in server.js

--- server.js
+++ server.js
@@ -16,11 +16,10 @@ server.get("/", (req, res) => {
 });
 
 const isValidEmail = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
 
-  
 //identify endpoint
 server.post("/identify", async (req, res) => {
   const { email, phoneNumber } = req.body;
@@ -37,15 +36,15 @@ server.post("/identify", async (req, res) => {
 
   try {
     const existingContacts = await prisma.contact.findMany({
-        where: {
-          OR: [
-            email ? { email } : undefined,
-            phoneNumber ? { phoneNumber } : undefined,
-          ].filter(Boolean), // Removes undefined conditions
-        },
-        orderBy: { createdAt: "asc" },
-      });
-
+      where: {
+        OR: [
+          email ? { email } : undefined,
+          phoneNumber ? { phoneNumber } : undefined,
+        ].filter(Boolean),
+      },
+      orderBy: { createdAt: "asc" },
+    });
+    // console.log(existingContacts);
     let primaryContact = null;
     let relatedContacts = [];
     let emails = [];
@@ -56,32 +55,53 @@ server.post("/identify", async (req, res) => {
       const primaryContacts = existingContacts.filter(
         (contact) => contact.linkPrecedence === "primary"
       );
-
-      if (primaryContacts.length > 1) {
+      // console.log(primaryContacts , "pcs")
+      if (primaryContacts.length === 0){
+        const secLinkedId = existingContacts[0].linkedId
+        const realPrimary = await prisma.contact.findUnique({where : {id : secLinkedId}})
+        const allContacts = await prisma.contact.findMany({where : {linkedId : realPrimary.id}})
+        const Nemails = allContacts.map(contact=>contact.email)
+        Nemails.push(realPrimary.email)
+        Nemails.sort()
+        const NphoneNumbers = allContacts.map(contact=>contact.phoneNumber)
+        const NsecondaryContactIds = allContacts.map(contact=>contact.id)
+        return res.status(200).json({
+          contact: {
+            primaryContactId: realPrimary.id,
+            emails : Nemails,
+            phoneNumbers : NphoneNumbers,
+            secondaryContactIds: NsecondaryContactIds,
+          },
+        })
+      }
+      if (primaryContacts.length > 0) {
         primaryContacts.sort((a, b) => a.createdAt - b.createdAt); // sorting by the time created
         primaryContact = primaryContacts[0]; // old one becomes the primary
 
- // converting other primary contact to secondary and re-linking their secondaries
+        // converting other primary contact to secondary and re-linking their secondaries
+        if (primaryContacts.length > 1) {
+          const otherPrimaryIds = primaryContacts
+            .slice(1)
+            .map((contact) => contact.id);
 
-  const otherPrimaryIds = primaryContacts.slice(1).map((contact) => contact.id);
-
-  await prisma.$transaction([
-    prisma.contact.updateMany({     //updaing the current primary contact to secondary contact
-      where: { id: { in: otherPrimaryIds } },
-      data: {
-        linkedId: primaryContact.id,
-        linkPrecedence: "secondary",
-      },
-    }),
-    // edge case where im handling the contact which is going to be the secondary contact,
-    // its secondary contacts now should point to the new primary contact
-    prisma.contact.updateMany({
-      where: { linkedId: { in: otherPrimaryIds } },
-      data: { linkedId: primaryContact.id },
-    }),
-  ]);
-} 
-    else {
+          await prisma.$transaction([
+            prisma.contact.updateMany({
+              //updaing the current primary contact to secondary contact
+              where: { id: { in: otherPrimaryIds } },
+              data: {
+                linkedId: primaryContact.id,
+                linkPrecedence: "secondary",
+              },
+            }),
+            // edge case where im handling the contact which is going to be the secondary contact,
+            // its secondary contacts now should point to the new primary contact
+            prisma.contact.updateMany({
+              where: { linkedId: { in: otherPrimaryIds } },
+              data: { linkedId: primaryContact.id },
+            }),
+          ]);
+        }
+      } else {
         primaryContact = primaryContacts[0];
       }
 
